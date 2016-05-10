9266faab36b69df3f5b0dd9d5b5b1fb04494ed12
Jim Newbery <james@tinnedfruit.com>
2016-05-10 13:24:03 +0100

    Basic test for layout at small screen size
    * Added layout test HTML page
    * Added layout spec with simple test for header and nav components
    * Added JS to hide nav
    * Shifted CSS around to different files

---
+++ dist/js/layout.js
@@ -0,0 +1,12 @@
+;(function() {
+
+  var nav = document.getElementById('primary-nav');
+  var HIDDEN_CLASS_NAME = 'nav--is-hidden';
+
+  function initNav() {
+    nav.classList.add(HIDDEN_CLASS_NAME);
+  }
+
+  initNav();
+
+})();
