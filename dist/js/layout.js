68983cbeef6bd867ef2e689eb963afa16d8ba2d5
Jim Newbery <james@tinnedfruit.com>
2016-05-11 11:33:44 +0100

    Add test for menu button displayed using JS

--- dist/js/layout.js
+++ dist/js/layout.js
@@ -1,10 +1,16 @@
 ;(function() {
 
   var nav = document.getElementById('primary-nav');
+  var header = document.getElementById('main-header');
+  var menuButton;
   var HIDDEN_CLASS_NAME = 'nav--is-hidden';
 
   function initNav() {
     nav.classList.add(HIDDEN_CLASS_NAME);
+    menuButton = document.createElement('button');
+    menuButton.className = 'button button--menu';
+    menuButton.textContent = 'Menu';
+    header.appendChild(menuButton);
   }
 
   initNav();
