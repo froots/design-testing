9963fc5c282bb2193ccf2fc7feb43dc4bb75e860
Jim Newbery <james@tinnedfruit.com>
2016-05-11 13:12:33 +0100

    Add test for nav open using Webdriver JS script

--- dist/js/layout.js
+++ dist/js/layout.js
@@ -4,13 +4,20 @@
   var header = document.getElementById('main-header');
   var menuButton;
   var HIDDEN_CLASS_NAME = 'nav--is-hidden';
+  var MENU_BUTTON_CLASS_NAME = 'button button--menu';
 
   function initNav() {
     nav.classList.add(HIDDEN_CLASS_NAME);
     menuButton = document.createElement('button');
-    menuButton.className = 'button button--menu';
+    menuButton.className = MENU_BUTTON_CLASS_NAME;
     menuButton.textContent = 'Menu';
+    menuButton.addEventListener('click', handleMenuClick);
     header.appendChild(menuButton);
+  }
+
+  function handleMenuClick(event) {
+    nav.classList.toggle(HIDDEN_CLASS_NAME);
+    event.preventDefault();
   }
 
   initNav();
